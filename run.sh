86b1687db5aa3b13df98a7720ddf4e3f5bfb2007
Diosa34 <superalex.osa@yandex.ru>
2022-03-26 23:43:53 +0300

    Changed path to jar in run.sh
    Version added to jar name

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 git pull
 chmod 777 gradlew
 ./gradlew build
-chmod 777 build/libs/lab5.jar
-java -jar build/libs/lab5.jar "$1"
+chmod 777 build/libs/lab5-1.0.jar
+java -jar build/libs/lab5-1.0.jar "$1"
