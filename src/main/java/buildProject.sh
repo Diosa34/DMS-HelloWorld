8e6a9d68cd8f3f2eeca0039aa5373bb2b5fd3fff
Diosa34 <superalex.osa@yandex.ru>
2022-03-26 23:01:26 +0300

    Change buildProject.sh
    Add ./gradlew build

--- src/main/java/buildProject.sh
+++ src/main/java/buildProject.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-rm -r lab5_prog/build/
-rm -r ObjectConverter/build/
-cd lab5_prog
 git pull
-./gradlew jar
-cd ../
-cd ObjectConverter
-git pull
-./gradlew jar
-cd ..
+chmod 777 gradlew
+./gradlew build
 chmod 777 lab5_prog/build/libs/lab5_prog.jar
-cp ObjectConverter/build/libs/ObjectConverter.jar lab5_prog/build/libs/
-chmod 777 lab5_prog/build/libs/ObjectConverter.jar
-cd lab5_prog/build/libs
-java -jar lab5_prog.jar "$1"
+java -jar lab5_prog/build/libs/lab5_prog.jar "$1"
